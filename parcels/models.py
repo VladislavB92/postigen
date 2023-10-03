2c06c909c38b3613f6bb1dd1aa871edb676ecf46
vlad <vladislavs@codnity.com>
2023-10-03 14:14:02 +0300

    Implement parcels and customers

--- parcels/models.py
+++ parcels/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from customers.models import Customer
 
-# Create your models here.
+PARCEL_SIZE_CHOICES = [
+	("XS", "Extra Small"),
+	("S", "Small"),
+	("M", "Medium"),
+	("L", "Large"),
+	("XL", "Extra Large"),
+]
+
+
+class Parcel(models.Model):
+	"""Parcel model."""
+
+	sender = models.ManyToManyField(
+		Customer,
+		related_name="sender",
+	)
+	receiver = models.ManyToManyField(
+		Customer,
+		related_name="receiver",
+	)
+	parcel_size = models.CharField(
+		max_length=11,
+		choices=PARCEL_SIZE_CHOICES,
+		null=False,
+		blank=False,
+	)
