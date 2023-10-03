1b706272d913417a09fba9c04c69811c0ccbfc7c
vlad <vladislavs@codnity.com>
2023-10-03 14:27:35 +0300

    Add Locker

---
+++ postigen/common_constants.py
@@ -0,0 +1,13 @@
+SIZE_CHOICES = [
+	("XS", "Extra Small"),
+	("S", "Small"),
+	("M", "Medium"),
+	("L", "Large"),
+	("XL", "Extra Large"),
+]
+
+STATUS_CHOICES = [
+	("free", "Free"),
+	("busy", "Busy"),
+	("out_of_order", "Out of Order"),
+]
