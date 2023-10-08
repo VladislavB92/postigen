42bc3b9d84a3829aaa84463be5756e9554d6fad8
vlad <vladislavs@codnity.com>
2023-10-09 02:17:05 +0300

    Add Admin title

--- customers/urls.py
+++ customers/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 from .views import (
 	CustomerListCreateView,
 	CustomerDetailView,
 )
 
+admin.site.site_header = "POSTIGEN"
 urlpatterns = [
 	path(
 		"",
